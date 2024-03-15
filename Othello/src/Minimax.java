285745c1563c9878b93129ae96e9813c71881127
Lene Borup Randløv <bora@itu.dk>
2024-03-15 10:32:44 +0100

    Initial alpha-beta pruning inplementation
    Co-authored-by: Johan Sandager Bennedsen <JohanSandager@users.noreply.github.com>

--- Othello/src/Minimax.java
+++ Othello/src/Minimax.java
@@ -4,43 +4,47 @@ public class Minimax implements IOthelloAI {
     @Override
     public Position decideMove(GameState state) {
         maxPlayer = state.getPlayerInTurn();
-        MiniMaxReturnType max = maxValue(state);
+        MiniMaxReturnType max = maxValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE);
         return max.move;
     }
 
-    private MiniMaxReturnType maxValue(GameState state) {
+    private MiniMaxReturnType maxValue(GameState state, int alpha, int beta) {
         if (state.isFinished() || state.legalMoves().isEmpty()) {
             return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
         }
-        int min = Integer.MIN_VALUE;
+        int value = Integer.MIN_VALUE;
         Position bestMove = new Position(-1, -1);
 
         for (Position pos : state.legalMoves()) {
-            MiniMaxReturnType minimum = minValue(result(state, pos));
-            if (minimum.miniMaxValue > min) {
-                min = minimum.miniMaxValue;
+            MiniMaxReturnType minimum = minValue(result(state, pos), alpha, beta);
+            if (minimum.miniMaxValue > value) {
+                value = minimum.miniMaxValue;
                 bestMove = pos;
+                alpha = Math.max(alpha, value);
             }
+            if(value >= beta) return new MiniMaxReturnType(value, bestMove);
         }
-        return new MiniMaxReturnType(min, bestMove);
+        return new MiniMaxReturnType(value, bestMove);
     }
 
-    private MiniMaxReturnType minValue(GameState state) {
+    private MiniMaxReturnType minValue(GameState state, int alpha, int beta) {
         if (state.isFinished() || state.legalMoves().isEmpty()) {
             return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
         }
-        int max = Integer.MAX_VALUE;
+        int value = Integer.MAX_VALUE;
         Position bestMove = new Position(-1, -1);
 
         for (Position pos : state.legalMoves()) {
 
-            MiniMaxReturnType maximum = maxValue(result(state, pos));
-            if (maximum.miniMaxValue < max) {
-                max = maximum.miniMaxValue;
+            MiniMaxReturnType maximum = maxValue(result(state, pos), alpha, beta);
+            if (maximum.miniMaxValue < value) {
+                value = maximum.miniMaxValue;
                 bestMove = pos;
+                beta = Math.min(beta, value);
             }
+            if(value <= alpha) return new MiniMaxReturnType(value, bestMove);
         }
-        return new MiniMaxReturnType(max, bestMove);
+        return new MiniMaxReturnType(value, bestMove);
     }
 
     private GameState result(GameState state, Position action) {
