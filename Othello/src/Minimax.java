4423d279cf6c493fa0a652ab76fb81096ec1aa02
Lene Borup Randløv <bora@itu.dk>
2024-03-15 11:18:20 +0100

    Depth limit in search implemented and working
    Co-authored-by: Johan Sandager Bennedsen <JohanSandager@users.noreply.github.com>
    Co-authored-by: Allan Andersen <allanzigge@users.noreply.github.com>

--- Othello/src/Minimax.java
+++ Othello/src/Minimax.java
@@ -1,22 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Minimax implements IOthelloAI {
     int maxPlayer;
-
     @Override
     public Position decideMove(GameState state) {
         maxPlayer = state.getPlayerInTurn();
-        MiniMaxReturnType max = maxValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        MiniMaxReturnType max = maxValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE, 8);
         return max.move;
     }
 
-    private MiniMaxReturnType maxValue(GameState state, int alpha, int beta) {
+    private MiniMaxReturnType maxValue(GameState state, int alpha, int beta, int depth) {
+        if(depth == 0) {
+            return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
+        }
         if (state.isFinished() || state.legalMoves().isEmpty()) {
             return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
         }
         int value = Integer.MIN_VALUE;
         Position bestMove = new Position(-1, -1);
 
-        for (Position pos : state.legalMoves()) {
-            MiniMaxReturnType minimum = minValue(result(state, pos), alpha, beta);
+        Set<Position> moves = new HashSet<Position>(); 
+        moves.addAll(state.legalMoves());
+
+        for (Position pos : moves) {
+            MiniMaxReturnType minimum = minValue(result(state, pos), alpha, beta, depth-1);
             if (minimum.miniMaxValue > value) {
                 value = minimum.miniMaxValue;
                 bestMove = pos;
@@ -27,16 +35,21 @@ public class Minimax implements IOthelloAI {
         return new MiniMaxReturnType(value, bestMove);
     }
 
-    private MiniMaxReturnType minValue(GameState state, int alpha, int beta) {
+    private MiniMaxReturnType minValue(GameState state, int alpha, int beta, int depth) {
+        if(depth == 0) {
+            return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
+        }
         if (state.isFinished() || state.legalMoves().isEmpty()) {
             return new MiniMaxReturnType(utility(state, maxPlayer), new Position(-1, -1));
         }
         int value = Integer.MAX_VALUE;
         Position bestMove = new Position(-1, -1);
 
-        for (Position pos : state.legalMoves()) {
+        Set<Position> moves = new HashSet<Position>(); 
+        moves.addAll(state.legalMoves());
 
-            MiniMaxReturnType maximum = maxValue(result(state, pos), alpha, beta);
+        for (Position pos : moves) {
+            MiniMaxReturnType maximum = maxValue(result(state, pos), alpha, beta, depth-1);
             if (maximum.miniMaxValue < value) {
                 value = maximum.miniMaxValue;
                 bestMove = pos;
@@ -48,7 +61,7 @@ public class Minimax implements IOthelloAI {
     }
 
     private GameState result(GameState state, Position action) {
-        GameState stateCopy = new GameState(state.getBoard(), state.getPlayerInTurn()); // black 1, white
+        GameState stateCopy = new GameState(state.getBoard(), state.getPlayerInTurn()); // black 1, white 2
         stateCopy.insertToken(action); // stateCopy.insertToken
         return stateCopy;
     }
